7e0d53decf00ebfbe81a324fb249794a13b79ba6
andy0207 <libinxusg@gmail.com>
2026-02-15 03:04:17 -0800

    .

--- train_vae.sh
+++ train_vae.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-#
-#SBATCH --job-name=ftdec
-#SBATCH --account=project_2001654
-#SBATCH -p gpusmall
-#SBATCH --gres=gpu:a100:1
-#SBATCH --time=24:00:00
-
-# Activate Conda environment
-
-source /scratch/project_2002846/Binxu/anaconda3/etc/profile.d/conda.sh
-# echo ". /scratch/project_2002846/Binxu/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
-conda activate binxu
-
 python ./opensora/train/train_causalvae.py \
     --exp_name "vae888_8_decoder_seg" \
     --batch_size 1 \
